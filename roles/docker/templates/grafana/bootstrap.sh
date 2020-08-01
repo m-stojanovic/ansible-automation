535cfa17d3e32634c6bff264f7a877d59b29f71a
Milos <56040933+m-stojanovic@users.noreply.github.com>
2020-08-01 19:11:57 +0200

    DevOps-4 Updating

--- roles/docker/templates/grafana/bootstrap.sh
+++ roles/docker/templates/grafana/bootstrap.sh
@@ -5,7 +5,8 @@ echo '1' > /proc/sys/net/ipv6/conf/lo/disable_ipv6
 echo '1' > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo '1' > /proc/sys/net/ipv6/conf/default/disable_ipv6
 
-/usr/sbin/grafana-server -config /etc/grafana/grafana.ini &
+service grafana-server start
+#/usr/sbin/grafana-server -config /etc/grafana/grafana.ini &
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
